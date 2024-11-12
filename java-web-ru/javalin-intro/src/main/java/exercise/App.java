4531f94a3b0715c624067a07bbb006f7c6e8a386
@hexlet/cli <support@hexlet.io>
2024-11-12 12:52:30 +0300

    submit java-web-ru/javalin-intro

---
+++ java-web-ru/javalin-intro/src/main/java/exercise/App.java
@@ -0,0 +1,22 @@
+package exercise;
+
+// BEGIN
+import io.javalin.Javalin;
+// END
+
+public final class App {
+
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> config.bundledPlugins.enableDevLogging());
+        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/welcome", ctx -> ctx.result("Welcome to Hexlet!"));
+        app.start(7070);
+        // Стартуем веб-сервер
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
