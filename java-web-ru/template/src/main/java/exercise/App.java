c8490b577cb3307033eda7fb8196193e9e674d97
@hexlet/cli <support@hexlet.io>
2024-11-14 11:08:08 +0300

    submit java-web-ru/template

--- java-web-ru/template/src/main/java/exercise/App.java
+++ java-web-ru/template/src/main/java/exercise/App.java
@@ -1,47 +1,49 @@
 package exercise;
 
 import io.javalin.Javalin;
-import java.util.List;
-import io.javalin.http.NotFoundResponse;
 import exercise.model.User;
 import exercise.dto.users.UserPage;
 import exercise.dto.users.UsersPage;
 import static io.javalin.rendering.template.TemplateUtil.model;
 import io.javalin.rendering.template.JavalinJte;
 
+import java.util.List;
+import java.util.Optional;
+
 public final class App {
 
-    // Каждый пользователь представлен объектом класса User
+    // Constant with the list of users
     public static final List<User> USERS = Data.getUsers();
 
     public static Javalin getApp() {
-
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
             config.fileRenderer(new JavalinJte());
         });
 
-        // BEGIN
-        // END
-
+        // Handler for listing users
         app.get("/users", ctx -> {
-    ctx.render("users/index.jte", model("USERS", USERS));
+            // Pass the list of users to the template
+            ctx.render("users/index.jte", model("USERS", USERS));
         });
 
+        // Handler for showing individual user details
         app.get("/users/{id}", ctx -> {
-            boolean found = false;
+            // Get user ID from the path parameter
             long id = ctx.pathParamAsClass("id", Long.class).get();
-            for(User user: USERS){
-                if (user.getId() == id){
-                    found = true;
-                    ctx.render("users/show.jte", model("user", user));
-                }
-            }
-            if (!found){
-                ctx.status(404).result("User not found");
+            Optional<User> userOpt = USERS.stream()
+                                          .filter(user -> user.getId() == id)
+                                          .findFirst();
+
+            if (userOpt.isPresent()) {
+                User user = userOpt.get();
+                // Pass user data to the template for rendering
+                ctx.render("users/show.jte", model("USER", user));
+            } else {
+                // If user not found, return 404 page
+                ctx.status(404).render("users/404.jte");
             }
         });
-
 
         return app;
     }
