e7f79df4e2031dd29d799ebafe4668fe8a1137c4
@hexlet/cli <support@hexlet.io>
2024-11-22 15:53:03 +0300

    submit java-web-ru/database

--- java-web-ru/database/src/main/resources/schema.sql
+++ java-web-ru/database/src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE products (
+CREATE TABLE IF NOT EXISTS products (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     price INT NOT NULL CHECK (price >= 0)
