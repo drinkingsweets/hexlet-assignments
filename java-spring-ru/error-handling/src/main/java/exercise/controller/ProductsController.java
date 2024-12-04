3da295257033cfcc3e6caa9adad8200c5b8bcfc7
@hexlet/cli <support@hexlet.io>
2024-12-04 14:57:02 +0300

    submit java-spring-ru/error-handling

--- java-spring-ru/error-handling/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-ru/error-handling/src/main/java/exercise/controller/ProductsController.java
@@ -38,6 +38,7 @@ public class ProductsController {
 
     // BEGIN
     @GetMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Product view(@PathVariable long id) {
         var product = productRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
