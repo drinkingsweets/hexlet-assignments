38311b7ba01180ea85c21e1129433902a2edb5ae
@hexlet/cli <support@hexlet.io>
2024-12-05 12:21:56 +0300

    submit java-spring-ru/jpa-comparing-entities

--- java-spring-ru/jpa-comparing-entities/src/main/java/exercise/handler/GlobalExceptionHandler.java
+++ java-spring-ru/jpa-comparing-entities/src/main/java/exercise/handler/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import exercise.exception.ResourceNotFoundException;
@@ -19,8 +20,9 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     // BEGIN
     @ExceptionHandler(ResourceAlreadyExistsException.class)
-    public ResponseEntity<String> handleResourceAlreadyExistsException(ResourceAlreadyExistsException ex){
-        return ResponseEntity.status(409).body(ex.getMessage());
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String handleResourceAlreadyExistsException(ResourceAlreadyExistsException e) {
+        return e.getMessage();
     }
     // END
 }
